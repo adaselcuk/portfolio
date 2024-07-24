5d704df4769e0e7fbb3b67cce82f2a63ef59dd67
Ada <adaselcuk16@gmail.com>
2024-07-24 20:25:55 +0300

    added divider btwn landing pg and about

--- script/script.js
+++ script/script.js
@@ -8,3 +8,24 @@ window.addEventListener('scroll', function() {
 		navbar.classList.add('initial-nav');
 	}
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var aboutLink = document.querySelector('a[href="#about"]');
+	var divider = document.querySelector('.about-divider');
+	var aboutSection = document.querySelector('#about');
+
+	aboutLink.addEventListener('click', function() {
+		event.preventDefault();
+		divider.classList.add('visible');
+		aboutSection.scrollIntoView({ behavior: 'smooth' });
+	});
+
+	window.addEventListener('scroll', function() {
+		var aboutSectionTop = aboutSection.getBoundingClientRect().top;
+		if (aboutSectionTop < window.innerHeight && aboutSectionTop >= 0) {
+			divider.classList.add('visible');
+		} else {
+			divider.classList.remove('visible');
+		}
+	});
+});
