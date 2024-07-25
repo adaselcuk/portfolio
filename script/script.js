107d1608bcae72008f721676461cd67e838b0373
Ada <adaselcuk16@gmail.com>
2024-07-25 21:07:26 +0300

    formatted abt page

--- script/script.js
+++ script/script.js
@@ -13,12 +13,35 @@ document.addEventListener('DOMContentLoaded', function() {
 	var aboutLink = document.querySelector('a[href="#about"]');
 	var divider = document.querySelector('.about-divider');
 	var aboutSection = document.querySelector('#about');
+	var projectsLink = document.querySelector('a[href="#projects"]');
+	var projectsSection = document.querySelector('#projects');
+	var contactLink = document.querySelector('a[href="#contact"]');
+	var contactSection = document.querySelector('#contact');
 
-	aboutLink.addEventListener('click', function() {
+	function scrollToSection(event, section) {
 		event.preventDefault();
-		divider.classList.add('visible');
-		aboutSection.scrollIntoView({ behavior: 'smooth' });
+		const headerOffset = 80;
+		const elementPosition = section.getBoundingClientRect().top;
+		const offsetPosition = elementPosition + window.scrollY - headerOffset;
+
+		window.scrollTo({
+			top: offsetPosition,
+			behavior: 'smooth'
+		});
+	}
+
+	aboutLink.addEventListener('click', function(event) {
+		scrollToSection(event, aboutSection);
 	});
+
+	contactLink.addEventListener('click', function(event) {
+		scrollToSection(event, contactSection);
+	});
+
+	projectsLink.addEventListener('click', function(event) {
+		scrollToSection(event, projectsSection);
+	});
+
 
 	window.addEventListener('scroll', function() {
 		var aboutSectionTop = aboutSection.getBoundingClientRect().top;
